eaba530f2bbf1797c18c5090ab3b33204a9b0452
Leandro Alves <leandro.alves351@gmail.com>
2022-11-07 22:52:35 -0300

    feat: trabalhando com pacotes

---
+++ 1-Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// Escrever registra uma mensagem na tela
+func escrever2() {
+	fmt.Println("Escrevendo pela função 2")
+}
---
+++ 1-Pacotes/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+func main() {
+	fmt.Println("Pacote batata!")
+	auxiliar.Escrever()
+
+}
