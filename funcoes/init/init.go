66f898e918b21da1bdd5594a4135c0c1379e11aa
Leandro Alves <leandro.alves351@gmail.com>
2022-11-18 01:07:19 -0300

    feat: função init

---
+++ funcoes/init/init.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Usa função init() por arquivo xpto.go
+func init() {
+	fmt.Println("Executando a função init")
+}
+
+func main() {
+	fmt.Println("Executando a função main")
+}
