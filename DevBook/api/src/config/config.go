d8cae4fba321143219dc1017d158999fb303ac02
Leandro Alves Magalhaes dos Santos <leandro.magalhaes@ifood.com.br>
2023-04-16 09:12:16 -0300

    chore: add new env vars

--- DevBook/api/src/config/config.go
+++ DevBook/api/src/config/config.go
@@ -29,9 +29,11 @@ func LoadingConfigs() {
 	}
 
 	StringConectionDB = fmt.Sprintf(
-		"%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
 	)
 
